07986f2ebe95bdae70f9f37678d087b34a904ef5
Harishmugi Thangamuthu <hthangamuthu@freshworks.com>
2024-09-02 14:32:50 +0530

    Initial Commit - Portfolio Website

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//  let Name="T.Harishmugi"
+// let english = 83;
+// let maths = 83;
+// if(english<maths){
+//     console.log("Maths mark is greater than English")
+// }else{console.log("Maths marks is lesser than English")
+
+// }
+// if(english==maths){console.log("Both are equal" )}
+
+
+let a = 1;
+if (a > 0) {
+    console.log("Positive")
+
+} else if (a < 0) { console.log("Negative") }
+else { console.log("Zero") }
+
+
+a = "i"
+if ((a == "a") || (a == "e") || (a == "i") || (a == "o") || (a == "u")) { console.log(" The charecter is vowel") }
+
+
+
+
+let spent = "$1"
+if (spent == "$10") { console.log("     Get 1 point") } else { spent="No points" }
+
+
+
+for (var count = 0; count < 3000; count++) {
+    if (spent == "No points") {
+        console.log("ooo")
+    }
+}
+// if (spent == "     Get 1 point") { console.log("eee") }
