26348255149da64f1463b1f785c9cfe6d9c13eb5
Harishmugi Thangamuthu <hthangamuthu@freshworks.com>
2024-09-03 14:23:34 +0530

    pic width change

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 
-//  let Name="T.Harishmugi"
-// let english = 83;
-// let maths = 83;
-// if(english<maths){
-//     console.log("Maths mark is greater than English")
-// }else{console.log("Maths marks is lesser than English")
+ let Name="T.Harishmugi"
+let english = 90;
+let maths =100;
+if(english>maths)
+    
+    
 
-// }
-// if(english==maths){console.log("Both are equal" )}
+    {
+    console.log("hhh")
+}
 
 
-let a = 1;
+else{console.log("mmm")
+
+}
+
+
+
+if(english==maths){console.log("Both are equal" )}
+else{  console.log("not equal")}
+
+
+let a = -1;
 if (a > 0) {
     console.log("Positive")
 
-} else if (a < 0) { console.log("Negative") }
+}
+ else if (a < 0) { console.log("Negative") }
+
 else { console.log("Zero") }
 
 
@@ -29,9 +42,21 @@ if (spent == "$10") { console.log("     Get 1 point") } else { spent="No points"
 
 
 
-for (var count = 0; count < 3000; count++) {
-    if (spent == "No points") {
-        console.log("ooo")
-    }
-}
+// for (var count = 0; count < 3000; count++) {
+//     if (spent == "No points") {
+//         console.log("ooo")
+//     }
+// }
 // if (spent == "     Get 1 point") { console.log("eee") }
+
+
+
+// Problem: John has taken a loan of $10,000 from a bank at an interest rate of 5% per annum for 3 years. Calculate the simple interest he has to pay at the end of the loan term.
+// Task: Use the formula Simple Interest = (P * R * T) / 100 where P = 10000, R = 5, and T = 3.;
+
+
+// let p=10000;
+// let r=5;
+// let t=3;
+// let simple_Interest=(p*r*t)/100 ;
+// console.log(simple_Interest)
