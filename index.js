049a8f60a330d166c4bf4126ea376a014bd44962
Harishmugi Thangamuthu <hthangamuthu@freshworks.com>
2024-09-05 13:48:13 +0530

    responsive edit 1

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 
- let Name="T.Harishmugi"
-let english = 90;
-let maths =100;
-if(english>maths)
-    
-    
-
-    {
-    console.log("hhh")
-}
-
-
-else{console.log("mmm")
-
-}
+//  let Name="T.Harishmugi"
+// let english = 90;
+// let maths =100;
+// if(english>maths)
 
 
 
-if(english==maths){console.log("Both are equal" )}
-else{  console.log("not equal")}
+//     {
+//     console.log("hhh")
+// }
 
 
-let a = -1;
-if (a > 0) {
-    console.log("Positive")
+// else{console.log("mmm")
 
-}
- else if (a < 0) { console.log("Negative") }
-
-else { console.log("Zero") }
-
-
-a = "i"
-if ((a == "a") || (a == "e") || (a == "i") || (a == "o") || (a == "u")) { console.log(" The charecter is vowel") }
+// }
 
 
 
+// if(english==maths){console.log("Both are equal" )}
+// else{  console.log("not equal")}
 
-let spent = "$1"
-if (spent == "$10") { console.log("     Get 1 point") } else { spent="No points" }
+
+// let a = -1;
+// if (a > 0) {
+//     console.log("Positive")
+
+// }
+//  else if (a < 0) { console.log("Negative") }
+
+// else { console.log("Zero") }
+
+
+// a = "i"
+// if ((a == "a") || (a == "e") || (a == "i") || (a == "o") || (a == "u")) { console.log(" The charecter is vowel") }
+
+
+
+
+// let spent = "$1"
+// if (spent == "$10") { console.log("     Get 1 point") } else { spent="No points" }
 
 
 
@@ -58,5 +58,68 @@ if (spent == "$10") { console.log("     Get 1 point") } else { spent="No points"
 // let p=10000;
 // let r=5;
 // let t=3;
-// let simple_Interest=(p*r*t)/100 ;
+// let simple_Interest=(p*r*t)/100 ; 
 // console.log(simple_Interest)
+
+
+
+// let mark = 100;
+
+// // First question
+// // for (var count = 0;count<1;count++ ){
+
+// if (mark >= 45) { console.log("Pass"); }
+// else { console.log("Fail"); }
+
+// // Second question
+
+
+// if (mark >= 60) {
+//     console.log("First Class");
+//     grade = "A";
+// }
+// else if (mark >= 45) {
+//     console.log("Second Class");
+//      grade = "B";
+// }
+// else {
+//     console.log("Fail");
+//      grade = "No Grade   ";
+// }
+
+// console.log("Grade : " + grade);
+// //     }
+// let letter = "a";
+// switch (letter ) {
+//     case "a":
+//         console.log("vowel");
+//         break;
+//     case "e":
+//         console.log("vowel");
+//         break;
+//     case "i"   :
+//         console.log("vowel");
+//         break;
+//     case "o":
+//         console.log("vowel");
+//         break;
+//     case "u":
+//         console.log("vowel");
+//         break;
+    
+// }
+
+// let age=15 ;
+
+// if(age<5){console.log("Free" );}
+// else if((age>=5)&&(age<=12)){console.log("10");}
+// else if((age>=13)&& (age<64)){console.log("20");}
+// else{console.log("15");}
+
+let a=10;
+let b=2;
+let c=3;
+
+if(a<b&&a<c){console.log(a);}
+else if(b<a&&b<c){console.log(b);}
+else {console.log(c);}
